6b6ede9fdc851cf1304a1ea05a63ee94d82d9449
Daria Moskalenko <daria.s.moskalenko@gmail.com>
2016-01-12 17:15:26 +0200

    add ability to change password

--- src/main/java/com/bionic/edu/PasswordChangeController.java
+++ src/main/java/com/bionic/edu/PasswordChangeController.java
@@ -1,13 +1,31 @@
 package com.bionic.edu;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @SessionScoped
 public class PasswordChangeController {
+	
+	@Autowired
+	private LoginController loginController;
+	
+	@Autowired
+	private UsersService userServise;
+	
+	@Autowired
+	private PasswordService passwordService;
+	
 	String oldPassword;
 	String newPassword;	
+
+	
+	public PasswordChangeController() {
+	}	
 	public String getOldPassword() {
 		return oldPassword;
 	}
@@ -20,13 +38,27 @@ public class PasswordChangeController {
 	public void setNewPassword(String newPassword) {
 		this.newPassword = newPassword;
 	}
-	public PasswordChangeController() {
-	}
-	
-	public String savePassword() {
-		
-		return null;
-	}
-	
 
+	public String savePassword() {
+		Users user = loginController.getUser();
+		FacesMessage message;
+		if (user != null) {
+			Users autUser = userServise.authorize(user.getLogin(), passwordService.encrypt(oldPassword));
+			if (autUser != null) {
+				user.setPassword(passwordService.encrypt(newPassword));
+				userServise.edit(user);
+				message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "INFO. The password was updated successfully", "The password was updated" );   
+				FacesContext.getCurrentInstance().addMessage(null, message);
+				return "schedule.xhtml?faces-redirect=true";				
+			} else {
+				message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR. Current password is wrong", "Current password is wrong" );   
+				FacesContext.getCurrentInstance().addMessage(null, message);
+				return null;				
+			}
+		} else {
+			message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR. Can't update the password", "The password wasn't updated" );   
+			FacesContext.getCurrentInstance().addMessage(null, message);
+			return null;
+		}
+	}
 }
