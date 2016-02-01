bb68aaf2f9e286652bf87c0a346c439646c4a3c9
Daria Moskalenko <daria.s.moskalenko@gmail.com>
2016-02-01 14:45:34 +0200

    add tests for HolidayServiceImplTest class

---
+++ src/test/java/holidaysht/HolidayServiceImplTest.java
@@ -0,0 +1,72 @@
+package holidaysht;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.epam.dao.HolidaysDao;
+import com.epam.entity.Holidays;
+import com.epam.services.HolidayService;
+import com.epam.services.HolidayServiceImpl;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Date;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HolidayServiceImplTest {
+	
+	@Mock
+	HolidaysDao holidaysDao;
+	
+	@InjectMocks
+	HolidayService holidayService = new HolidayServiceImpl();
+	
+	@Test
+	public void saveTest() {
+		Holidays holiday = new Holidays();
+		holidayService.save(holiday);
+		verify(holidaysDao).save(holiday);
+	}
+	
+	@Test
+	public void editTest() {
+		Holidays holiday = new Holidays();
+		holidayService.edit(holiday);
+		verify(holidaysDao).edit(holiday);
+	}
+	
+	@Test
+	public void deleteTest() {
+		Holidays holiday = new Holidays();
+		holidayService.delete(holiday);
+		verify(holidaysDao).delete(holiday);
+	}
+	
+	@Test
+	public void findByIdTest() {
+		when(holidaysDao.findById(anyInt())).thenReturn(null);
+		assertNull(holidayService.findById(anyInt()));
+		verify(holidaysDao).findById(anyInt());
+	}
+	
+	@Test
+	public void findByUserDateTest() {
+		when(holidaysDao.findByUserDate(anyString(), any(Date.class))).thenReturn(null);
+		assertNull(holidayService.findByUserDate(anyString(), any(Date.class)));
+		verify(holidaysDao).findByUserDate(anyString(), any(Date.class));
+		
+	}
+	
+	@Test
+	public void getAllTest() {
+		when(holidaysDao.getAll()).thenReturn(null);
+		assertNull(holidayService.getAll());
+		verify(holidaysDao).getAll();
+	}
+
+}
